4383d37bd641895a2992b50cd0801fd84d5c1b05
Vincent Martin <tu5xxx@temple.edu>
2014-02-08 23:01:47 -0500

    first update of the bcd_add_datapath

--- xilinx/lab2/bcd_add_datapath.v
+++ xilinx/lab2/bcd_add_datapath.v
@@ -40,5 +40,65 @@ module bcd_add_datapath(
     output BCD_DISPLAY_RESULT_MS_ACK
     );
 
+    //Setup an A and B register to hold the values to add.
+    reg [7:0] A;
+    reg [7:0] B;
+    reg [15:0] ABResult;
+
+    always@(posedge CLK)
+        begin
+            
+            //Clear the input and output values.
+            if(BCD_INIT) 
+                begin
+                    A = 0;
+                    B = 0;
+                    ABResult = 0;
+                    BCD_INIT = 0;
+                    BCD_INIT_ACK = 1;
+                end
+
+            //Load the A value.
+            if(BCD_LOAD_A)
+                begin
+                    A = SW[7:0];
+                    BCD_LOAD_A = 0;
+                    BCD_LOAD_A_ACK = 1;
+                end
+
+            //Load the B value.
+            if(BCD_LOAD_B)
+                begin
+                    B = SW[7:0];
+                    BCD_LOAD_B = 0;
+                    BCD_LOAD_B_ACK = 1;
+                end
+
+            //Display the A value on the LED in BCD format.
+
+            //Display the B value on the LED in BCD format.
+
+            //Add A and B
+            if(BCD_ADD)
+                begin
+                    ABResult = A + B;
+                    BCD_ADD = 0;
+                    BCD_ADD_ACK = 1;
+                end
+
+            IF(BCD_DISPLAY_RESULT_LS)
+                begin
+
+                end
+
+            IF(BCD_DISPLAY_RESULT_MS)
+                begin
+
+                end
+
+
+
+
+        end //End of always block.
 
 endmodule
